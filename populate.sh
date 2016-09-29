10626ca5ad02457069d763ce023b9035e7caa30c
James Cherven <jcherven@mail.usf.edu>
2016-09-29 00:35:48 -0400

    Refactors the download function and error checking

--- populate.sh
+++ populate.sh
@@ -6,20 +6,21 @@
 #### Constants
 
 targetdir=$1
-url1="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.4.22.tar.xz"
-url2="https://cdn.kernel.org/pub/linux/kernel/v3.x/linux-3.18.42.tar.xz"
-package1="linux-4.4.22.tar.xz"
-package2="linux-3.18.42.tar.xz"
+url1="rsync://rsync.kernel.org/pub/linux/kernel/v4.x/linux-4.7.tar.xz"
+#url2="rsync://rsync.kernel.org/pub/linux/kernel/v3.x/linux-3.19.tar.xz"
+package1="linux-4.7.tar.xz"
+#package2="linux-3.19.tar.xz"
 
 #### Functions
 
-## Download the tarball if it exists.
+## Download the tarball if it doesn't exist, skip downloading if it does.
 ## Accepts one of the constant variables for the tarball link.
 download_tarball()
 {
-    if [ ! -e  $1 ]
+    if [ ! -e "$1" ]
     then
-        wget $1
+            # hit the rsync daemon, suppress the MOTD, show progress, and skip if the file exists
+            rsync --no-motd -uP "$1" "$(pwd)"
     fi
     return 0
 }
@@ -29,9 +30,12 @@ download_tarball()
 ## Accepts one of the variables for the package name
 extract_tarball()
 {
-    if [ -e $1 ]
+    if [ -e "$1" ]
     then
-        tar -xvf $1
+        tar -xvf "$1"
+    else
+        echo "Could not extract, archive did not download properly (aborting)"
+        return 1
     fi
     return 0
 }
@@ -39,18 +43,18 @@ extract_tarball()
 #### Main logic
 if [[ $# -eq 0 ]]
 then
-        echo "Target directory must be specified as an absolute path (aborting)"
+        echo "Target directory must be specified (aborting)"
         exit 1
 else
     ## Enter the target directory
-    pushd $1
+    pushd "$targetdir"
 
-    download_tarball $url1
+    download_tarball "$url1"
 
-    extract_tarball $package1
+    extract_tarball "$package1"
 
     ## Delete the downloaded archive after extracting
-    rm -f $package1
+    rm -f "$package1"
 
     ## Exit the target directory
     popd
