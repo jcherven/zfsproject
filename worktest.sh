53d63a1da079016a92be2569fb22135228b432dd
James Cherven <jcherven@mail.usf.edu>
2016-10-02 16:39:42 -0400

    worktest is expanded to include downloading & extracting patches

--- worktest.sh
+++ worktest.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
+
+set -x
  
-for major in {0..20}; do
-    for minor in {0..100}; do
-        curl --head --silent --fail --list-only https://www.kernel.org/pub/linux/kernel/v3.0/patch-3."$major"."$minor".gz
-    done
+for minor in {0..10}; do
+    if curl --head --silent --fail --list-only https://www.kernel.org/pub/linux/kernel/v3.0/patch-3.0."$minor".gz
+    then
+         rsync --no-motd -uP rsync://rsync.kernel.org/pub/linux/kernel/v3.0/patch-3.0."$minor".gz ~/testdir
+         gunzip ~/testdir/patch-3.0."$minor".gz
+    fi
+    
 done
