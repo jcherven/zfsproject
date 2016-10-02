c5f804f503b897af7d13609e384b7d55c1be983f
James Cherven <jcherven@mail.usf.edu>
2016-10-02 17:10:57 -0400

    refactors

--- worktest.sh
+++ worktest.sh
@@ -2,11 +2,18 @@
 
 set -x
  
+url="https://www.kernel.org/pub/linux/kernel/v3.0/"
+rsyncurl="rsync://rsync.kernel.org/pub/linux/kernel/v3.0/"
+targetdir=~/testdir/
+patchver="patch-3.0."
+
 for minor in {0..10}; do
-    if curl --head --silent --fail --list-only https://www.kernel.org/pub/linux/kernel/v3.0/patch-3.0."$minor".gz
+    if curl --head --silent --fail --list-only "$url""$patchver""$minor".gz
     then
-         rsync --no-motd -uP rsync://rsync.kernel.org/pub/linux/kernel/v3.0/patch-3.0."$minor".gz ~/testdir
-         gunzip ~/testdir/patch-3.0."$minor".gz
+        rsync --no-motd -uP "$rsyncurl""$patchver""$minor".gz "$targetdir"
+        gunzip ~/testdir/"$patchver""$minor".gz
     fi
     
 done
+
+exit 0
