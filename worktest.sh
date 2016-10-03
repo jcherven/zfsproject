f20dc5a5b953eb6bdd56c7adfa36c0d1a968ab08
James Cherven <jcherven@mail.usf.edu>
2016-10-02 21:47:24 -0400

    greatly improves the running time of the workload test

--- worktest.sh
+++ worktest.sh
@@ -4,23 +4,25 @@
 ## downloading and patching the Linux 3.0.X source tree.
 ## Usage: ./populate.sh {path to target directory}
 
-#set -x
+set -x
  
 linuxver="3.0"
 url=https://www.kernel.org/pub/linux/kernel/v"$linuxver"/incr/
 rsyncurl=rsync://rsync.kernel.org/pub/linux/kernel/v"$linuxver"/incr/
-targetdir="$1"
+targetdir=~/testdir/linux
 patchver=patch-"$linuxver"
 
 pushd "$targetdir"
 
-for current in {0..101}; do
-    for incremental in {0..101}; do   
+for current in {4..101}; do
+        ## Use a C-style loop so that a variable can be used/set in the incremental version number
+        for (( incremental = current ; incremental <= 101 ; incremental += 1 )); do  
         if curl --head --silent --fail --list-only "$url""$patchver"."$current"-"$incremental".gz
         then
             rsync --no-motd -uP "$rsyncurl""$patchver"."$current"-"$incremental".gz "$targetdir"
-            gunzip "$patchver"."$current"-"$incremental".gz
-            patch -p1 < "$targetdir""$patchver"."$current"-"$incremental"
+            gunzip "$targetdir"/"$patchver"."$current"-"$incremental".gz
+            patch -p1 < "$targetdir"/"$patchver"."$current"-"$incremental"
+            break
     fi
     done    
 done
