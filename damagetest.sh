cb27ea06b874adbef2c89974df66f2cfe127ca41
James Cherven <jcherven@mail.usf.edu>
2016-10-16 15:07:44 -0400

    Adds things

--- damagetest.sh
+++ damagetest.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
-
+## Usage: ./damage.sh [ZPOOL] [DAMAGE LEVEL{1,2,3}]
 set -x
 
 ## Global variables
-zpool="anaheim"
-
+zpool="$1"
+damagelevel="$2"
 ## Available disks
 disks=('/dev/sda' '/dev/sdb' '/dev/sdc' '/dev/sdd')
-
-## Get available device size in blocks for shuf command from blockdev 
 
 ## export the zpool to keep ZFS from self-healing the damage
 #zpool export "$zpool"
 
 ##corrupt the raw disk
 
-# Pick a random disk to $targetdisk
-disknum=$(( $RANDOM % 4 ))
+# Target a random disk for corruption 
+# Get a random element from $disks
+disknum=$(($RANDOM % 4))
 targetdisk=${disks[disknum]}
+# Get the maximum block number of $targetdisk as an upperbound for $targetblock
+blocksize=$(blockdev --getbsz $targetdisk)
 
-# Pick a random block to $targetblock
-upperbound=$(blockdev --report | awk -v var="$targetdisk$" '$7 ~ var {print $6}')
-targetblock=$(shuf --input-range=1-$upperbound --head-count=1)
-echo "Target disk is now $targetdisk, target block is now $targetblock"
-    ## run a loop that chooses a random disk and seeks to a random block,
-    ## writing garbage onto it from /dev/urandom
-    ## 
+## Target a random block from $targetdisk for corruption
+for i in 10; do        
+    upperbound=$(blockdev --report | awk -v var="$targetdisk$" '$7 ~ var {print $6}')
+    targetblock=$(shuf --input-range=1-$upperbound --head-count=1)
+    echo "Target disk is now $targetdisk, target block is now $targetblock"
+done
+
+# Seeks to the target block on the target disk, then writes one block of garbage over it
+# dd bs="$blocksize" count=1 seek="$targetblock" if=/dev/urandom of="$targetdisk"
+
+
 ## import the zpool
 #zpool import "$zpool"
 
