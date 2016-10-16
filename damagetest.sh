19ad417c8144501b93f75100e4ff0e18a6b50426
James Cherven <jcherven@mail.usf.edu>
2016-10-16 14:40:36 -0400

    debugs

--- damagetest.sh
+++ damagetest.sh
@@ -11,25 +11,23 @@ disks=('/dev/sda' '/dev/sdb' '/dev/sdc' '/dev/sdd')
 ## Get available device size in blocks for shuf command from blockdev 
 
 ## export the zpool to keep ZFS from self-healing the damage
-zpool export "$zpool"
+#zpool export "$zpool"
 
 ##corrupt the raw disk
 
 # Pick a random disk to $targetdisk
 disknum=$(( $RANDOM % 4 ))
 targetdisk=${disks[disknum]}
-echo "target disk is $targetdisk"
 
 # Pick a random block to $targetblock
 upperbound=$(blockdev --report | awk -v var="$targetdisk$" '$7 ~ var {print $6}')
 targetblock=$(shuf --input-range=1-$upperbound --head-count=1)
-echo "targetblock is $targetblock"
+echo "Target disk is now $targetdisk, target block is now $targetblock"
     ## run a loop that chooses a random disk and seeks to a random block,
     ## writing garbage onto it from /dev/urandom
     ## 
 ## import the zpool
-zpool import "$zpool"
-echo "$zpool is imported"
+#zpool import "$zpool"
 
 ## scrub the zpool and display results of corruption
 ##zpool scrub "$zpool"
