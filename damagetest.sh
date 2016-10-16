7c2e9330e85669cc9c646c08afec39baab7918f6
James Cherven <jcherven@mail.usf.edu>
2016-10-15 22:20:52 -0400

    Adds a test of the disk corrupting script

---
+++ damagetest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set x
+
+## Global variables
+targetdisk="$1"
+zpool="anaheim"
+disk01="/dev/sda"
+disk02="/dev/sdb"
+disk03="/dev/sdc"
+disk04="/dev/sdd"
+
+
+## Get available device size in blocks for shuf command from blockdev 
+upperbound=$(blockdev --report | awk '$7 ~ '"$targetdisk"$' {print $6}') 
+echo "upperbound is $upperbound"
+## export the zpool to keep ZFS from self-healing the damage
+export "$zpool"
+echo "$zpool is exported"
+##corrupt the raw disk
+#for i in 24 do
+        targetblock=shuf --input-range=1-$upperbound --head-count=1
+        echo "targetblock is $targetblock"
+    ## run a loop that chooses a random disk and seeks to a random block,
+    ## writing garbage onto it from /dev/urandom
+    ## 
+
+## import the zpool
+import "$zpool"
+echo "$zpool is imported"
+
+## scrub the zpool and display results of corruption
+zpool scrub "$zpool"
+zpool status -v "$zpool"
+
+exit 0
