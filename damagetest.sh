f66c50fb67215f94b341ab25f8fe2947094267d4
James Cherven <jcherven@mail.usf.edu>
2016-10-16 14:33:27 -0400

    Adds disk randomization to damage test

--- damagetest.sh
+++ damagetest.sh
@@ -3,34 +3,36 @@
 set -x
 
 ## Global variables
-targetdisk="$1"
 zpool="anaheim"
-disk01="/dev/sda"
-disk02="/dev/sdb"
-disk03="/dev/sdc"
-disk04="/dev/sdd"
 
+## Available disks
+disks=(disk01 disk02 disk03 disk04)
 
 ## Get available device size in blocks for shuf command from blockdev 
-upperbound=$(blockdev --report | awk -v var="$disk01$" '$7 ~ var {print $6}') 
-echo "upperbound is $upperbound"
+
 ## export the zpool to keep ZFS from self-healing the damage
 zpool export "$zpool"
-echo "$zpool is exported"
+
 ##corrupt the raw disk
-#for i in 24 do
+
+# Pick a random disk to $targetdisk
+disknum=$(( $RANDOM % 4 ))
+targetdisk=${disks[disknum]}
+echo "target disk is $targetdisk"
+
+# Pick a random block to $targetblock
+upperbound=$(blockdev --report | awk -v var="$disk01$" '$7 ~ var {print $6}')
 targetblock=$(shuf --input-range=1-$upperbound --head-count=1)
-        echo "targetblock is $targetblock"
+echo "targetblock is $targetblock"
     ## run a loop that chooses a random disk and seeks to a random block,
     ## writing garbage onto it from /dev/urandom
     ## 
-
 ## import the zpool
 zpool import "$zpool"
 echo "$zpool is imported"
 
 ## scrub the zpool and display results of corruption
-zpool scrub "$zpool"
-zpool status -v "$zpool"
+##zpool scrub "$zpool"
+##zpool status -v "$zpool"
 
 exit 0
