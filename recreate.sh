cb0912fa6f719f229804618ebf62615e8d8e8b1a
James Cherven <jcherven@mail.usf.edu>
2016-10-18 23:48:56 -0400

    Fixes child/parent shell exit problem calling other scripts

--- recreate.sh
+++ recreate.sh
@@ -8,7 +8,7 @@ zpool create -f anaheim mirror disk01 disk02 mirror disk03 disk04
 zfs create anaheim/data
 
 source ./populate.sh /anaheim/data
-
+source ./workload.sh /anaheim/data
 zpool status -v
-
+unalias exit
 exit 0
