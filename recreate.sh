391df47d178b51e3684affc77e46fd4e357b1c84
James Cherven <jcherven@mail.usf.edu>
2016-10-18 23:20:27 -0400

    Adds some extras to the recreation script, renames it to recreate.sh

---
+++ recreate.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -x
+
+zpool destroy anaheim
+zpool create anaheim mirror disk01 disk02 mirror disk03 disk04
+zfs create anaheim anaheim/data
+
+source ./populate.sh anaheim && source ./workload.sh /anaheim/data
+exit 0
