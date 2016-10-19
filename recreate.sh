1531c4de60043da9670b11f1964df2fd5bf72c7a
James Cherven <jcherven@mail.usf.edu>
2016-10-18 23:34:31 -0400

    Fixes a bad path

--- recreate.sh
+++ recreate.sh
@@ -4,8 +4,7 @@ set -x
 
 zpool destroy anaheim
 zpool create -f anaheim mirror disk01 disk02 mirror disk03 disk04
-zfs create anaheim
 zfs create anaheim/data
 
-source ./populate.sh anaheim/data && source ./workload.sh /anaheim/data
+source ./populate.sh /anaheim/data && source ./workload.sh /anaheim/data
 exit 0
