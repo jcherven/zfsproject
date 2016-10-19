45cbe4af02c3b0f38d184171989f4d5fc57b2a11
James Cherven <jcherven@mail.usf.edu>
2016-10-18 23:36:54 -0400

    Adds status output to recreate.sh

--- recreate.sh
+++ recreate.sh
@@ -7,4 +7,6 @@ zpool create -f anaheim mirror disk01 disk02 mirror disk03 disk04
 zfs create anaheim/data
 
 source ./populate.sh /anaheim/data && source ./workload.sh /anaheim/data
+
+zpool status -v
 exit 0
