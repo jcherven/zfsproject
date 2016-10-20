2f10e740e128a991eb83c74e162d524c7bb233a9
James Cherven <jcherven@mail.usf.edu>
2016-10-19 21:43:23 -0400

    corrects source command in recreate.sh

--- recreate.sh
+++ recreate.sh
@@ -6,8 +6,8 @@ zpool destroy anaheim
 zpool create -f anaheim mirror disk01 disk02 mirror disk03 disk04
 zfs create anaheim/data
 alias exit=return
-source ./populate.sh /anaheim/data
-source ./workload.sh /anaheim/data
+source populate.sh /anaheim/data
+source workload.sh /anaheim/data
 zpool status -v
 unalias exit
 exit 0
